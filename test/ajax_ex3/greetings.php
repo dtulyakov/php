f23cc547ab78d7cbecd21e975ff7021e5f7a63dc
Денис Туляков <dtulyakov@gmail.com>
2014-12-05 23:19:16 +0300

    В ветке master
     Изменения для закрепления:
    	deleted:    css/l2.css
    	deleted:    css/style.css
    	deleted:    favicon.ico
    	deleted:    index.php
    	deleted:    jquery.js
    	deleted:    test.php
    	deleted:    test/ajax_ex1/index.php
    	deleted:    test/ajax_ex1/jquery.js
    	deleted:    test/ajax_ex1/time.php
    	deleted:    test/ajax_ex2/index.html
    	deleted:    test/ajax_ex2/jquery.js
    	deleted:    test/ajax_ex2/page1.html
    	deleted:    test/ajax_ex2/page2.html
    	deleted:    test/ajax_ex3/greetings.php
    	deleted:    test/ajax_ex3/index.html
    	deleted:    test/ajax_ex3/jquery.js
    	deleted:    test/example1/example1.html
    	deleted:    test/example1/jquery.js
    	deleted:    test/example1/script.js
    	deleted:    test/example2/example2.html
    	deleted:    test/example2/jquery.js
    	deleted:    test/example2/script.php
    	deleted:    test/rezina.html
    	deleted:    test/rezina2.php
    	deleted:    test/top-cmd.php
    	deleted:    test/top.php
    	deleted:    test/trassa-cmd1.php
    	deleted:    test/trassa-cmd2.php
    	deleted:    test/trassa.php
    	deleted:    timej.php
    
     Несопровождаемые файлы:
    	old/

--- test/ajax_ex3/greetings.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	echo "Приветствую, <b>".$_REQUEST['username']."</b>!<br>";
-	echo "В вашем имени букв: ".strlen($_REQUEST['username']).".";
- ?>
--- test/example2/script.php
+++
@@ -1,5 +0,0 @@
-<?php
-	header('Content-Type: text/javascript; charset=UTF-8');
-	$rnd = rand(1,100);		// генерируем случайное число от 1 до 100
-	echo '$("#content").html($("#content").html()+'.$rnd.'+"<BR/>");';	// выдаем код скрипта в поток вывода
-?>
